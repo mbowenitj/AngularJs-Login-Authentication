68cc68cb0af89117e039615ccecbbc7431767310
Tshepo Mboweni <tshepom@mannmademedia.com>
2017-01-25 15:45:47 +0200

    eliminated all errors

--- src/app/projects/projects.controller.js
+++ src/app/projects/projects.controller.js
@@ -15,8 +15,15 @@ angular.module('tangentLoginProject')
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
+
+              console.log("success, ",response);
               $scope.projects = response;
+
+
           }, function errorCallback(response) {
+
+
+            console.log("failure, ",response);
             // called asynchronously if an error occurs
             // or server returns response with an error status.
           });
