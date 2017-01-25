de80ed5051961e55bf8f55829e35ee571c80baa6
Tshepo Mboweni <tshepom@mannmademedia.com>
2017-01-25 17:41:38 +0200

    listing of projects completed

--- src/app/projects/projects.controller.js
+++ src/app/projects/projects.controller.js
@@ -7,27 +7,34 @@ angular.module('tangentLoginProject')
   var vm = this;
   vm.projects = [];
 
+  var tsk = this;
+  tsk.task = [];
+
         $http({
           method: 'GET',
             url: 'http://projectservice.staging.tangentmicroservices.com/api/v1/projects/',
             headers: {
-                      'Authorization': 'Token b7ec34e136bb6d28a4421e422e852b99cc834d17'
+                      'Authorization': 'Token b7ec34e136bb6d28a4421e422e852b99cc834d17',
+                      'Content-type': 'application/json'
                      }
-        }).then(function successCallback(response) {
+        })
+
+        .then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
 
               console.log("success, ",response);
-              $scope.projects = response.project;
-
+              $scope.projects = response.data;
+              $scope.task = response.data;
 
           }, function errorCallback(response) {
-
 
             console.log("failure, ",response);
             // called asynchronously if an error occurs
             // or server returns response with an error status.
           });
+
+
 
 }]);
 
