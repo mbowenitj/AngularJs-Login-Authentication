b8d87b3c57a03fffe828e61e951980ce35cc0344
tshepo <mbowenitshepo@gmail.com>
2017-01-24 20:16:14 -0800

    modified login controller and service

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,17 +13,13 @@
         templateUrl: 'app/login/login.html',
         controller: 'LoginController'
       });
-      // .state('projects', {
-      //   url: '/projects',
-      //   templateUrl: 'app/projects/projects.html',
-      //   controller: 'SubmittedController'
-      // });
 
     $urlRouterProvider.otherwise('/');
   }
 
   run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
     function run($rootScope, $location, $cookies, $http) {
+
         // keep user logged in after page refresh
         $rootScope.globals = $cookies.getObject('globals') || {};
         if ($rootScope.globals.currentUser) {
@@ -32,7 +28,7 @@
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
             var loggedIn = $rootScope.globals.currentUser;
             if (restrictedPage && !loggedIn) {
                 $location.path('/login');
--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,36 +1,23 @@
-(function() {
-  'use strict';
+'use strict';
 
-  angular
-    .module('tangentLoginProject')
-    .controller('LoginController', MainController);
+angular.module('tangentLoginProject')
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-      function LoginController($location, AuthenticationService, FlashService) {
-          var usr = this;
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
 
-          usr.login = login;
-
-          (function initController() {
-              // reset login status
-              AuthenticationService.ClearCredentials();
-          })();
-
-          function login() {
-              usr.dataLoading = true;
-              AuthenticationService.Login(usr.username, usr.password, function (response) {
-                  if (response.success) {
-                      AuthenticationService.SetCredentials(usr.username, usr.password);
-                      $location.path('/');
-                  } else {
-                      FlashService.Error(response.message);
-                      usr.dataLoading = false;
-                  }
-              });
-          };
-      }
-
-
-
-
-});
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
